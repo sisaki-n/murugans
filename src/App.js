3ab09d422d1843e095c7d50966273ad312c67c1a
sisaki-n <sisaki.n@gmail.com>
2019-09-02 16:48:57 +0530

    adding header component and links to logos n options

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { HomePage } from './pages/homepage/homepage.component';
 import './App.css';
-import {Route} from 'react-router-dom';
+import {Route,Switch} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './Components/header/header.component';
 
 
 function App() {
   return (
-    <div className="App">    
+    <div className="App">   
+    <Header/>
+    <Switch>
     <Route exact path= '/' component={HomePage}></Route>
     <Route path ="/shop" component={ShopPage}></Route>
+    </Switch> 
     </div>
   );
 }
