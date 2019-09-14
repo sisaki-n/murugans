b0cbda5708445003ff58714ede89ed21ae72acfe
sisaki-n <sisaki.n@gmail.com>
2019-09-14 17:45:17 +0530

    sign in with email and password

--- src/App.js
+++ src/App.js
@@ -32,9 +32,7 @@ class App extends React.Component {
              id: snapshot.id,
              ...snapshot.data()
         }
-      });
-
-         console.log(this.state);
+      });         
     }); 
 
      }
