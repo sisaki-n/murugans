28faa34c3c446e9217516ba6336b69800e9ef5da
sisaki-n <sisaki.n@gmail.com>
2019-09-14 17:05:22 +0530

    sign up page creation

--- src/pages/SignInAndSignUp/SignInAndSignUpPage.Component.jsx
+++ src/pages/SignInAndSignUp/SignInAndSignUpPage.Component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './SignInAndSignUpPage.Styles.scss';
 import SignIn from '../../Components/Sign-In/signin.component';
+import SingUp from '../../Components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => (
-    <div className="Sign-in-and-sing-up">
+    <div className="sign-in-and-sing-up">
     <SignIn/>
+    <SingUp/>
     </div>
 );
 
