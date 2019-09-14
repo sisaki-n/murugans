5cde9b72e680502b39de3853c974d0da06ce2f58
sisaki-n <sisaki.n@gmail.com>
2019-09-14 15:51:27 +0530

    adding google sign into firebase database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Route,Switch} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './Components/header/header.component';
 import SignInAndSignUpPage from './pages/SignInAndSignUp/SignInAndSignUpPage.Component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -22,9 +22,25 @@ class App extends React.Component {
 
   componentDidMount(){
 
-    this.unSubscribeFromAuth =auth.onAuthStateChanged( user => {
-      this.setState({currentUser: user});
-      console.log(user);
+    this.unSubscribeFromAuth =auth.onAuthStateChanged( async userAuth => {
+     if(userAuth){
+      const userRef = await createUserProfileDocument(userAuth);
+
+      userRef.onSnapshot( snapshot =>
+        this.setState({ 
+          currentUser:{
+             id: snapshot.id,
+             ...snapshot.data()
+        }
+      })
+      );
+
+     }
+     else{
+       this.setState({currentUser:userAuth});
+     }
+
+   
     });
   }
 
